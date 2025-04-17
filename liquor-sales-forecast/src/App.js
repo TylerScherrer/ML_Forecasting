9cb9bd1e2dd6cacf162d0dba520f840e94d1e0d1
Tyler <Tyler_Scherrer00@yahoo.com>
2025-04-16 19:59:28 -0500

    requirements.txt

--- liquor-sales-forecast/src/App.js
+++ liquor-sales-forecast/src/App.js
@@ -19,9 +19,9 @@ import SeasonalitySummaryCard from "./components/SeasonalitySummaryCard";
 import FeatureImportanceSummaryCard from "./components/FeatureImportanceSummaryCard";
 import AiForecastSummaryCard from "./components/AiForecastSummaryCard";
 
-// ✅ Auto-switch between localhost and production
+
 const BASE_URL = "https://ml-forecast-api-bpa9g0hscaccc0e0.canadacentral-01.azurewebsites.net";
-// App.js — add a log line temporarily
+
 console.log("Forcing redeploy");
 
 function App() {
